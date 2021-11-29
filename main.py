db59a1aa3d105eb5f19d190114beb7d52c19fecd
ismael.matamoros <incaICE2614MUC!>
2021-11-29 10:07:49 +0100

    dev: first version of the main script and file with FMP quote data from 29/11/2021 - main.py gets all FMP tickers with financial data available and generates a pandas DataFrame with all available FMP quotes. Quote data can be saved to a csv file for later use. Quote data can be either retrieved from the FMP API or loaded from an existing csv file. - A csv file with FMP quotes DataFrame from 29/11/2021 is added.

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-import matplotlib.pyplot as plt
-import numpy as np
+from functions.retrieve_data_fmp import *
 
-x = np.array(np.linspace(0, 2*3.14, num=1000))
-y = np.sin(x)
 
-plt.scatter(x, y)
-plt.show()
+# ---------------------- Configuration ----------------------
+config = dict()
+config["retrieve_fmp_data"] = True  # Generate data from FMP
+config["save_fmp_data"] = True  # Save FMP quote data to CSV file
+config["fmp_quotes_file"] = "fmp_quotes_2111282042.csv"  # CSV file to load FMP quote data
+
+# ---------------------- From FMP ----------------------
+# FMP API keys
+api_key_fmp = "1d5f2b76686c208084ad943923c6c8c3"  # ismaelmatamoros@gmail.com
+# api_key_fmp = "dea2bd3cdbfe04bb4d482760055043c9"  # ismaelmatamoros@hotmail.com
+
+# Generate or load raw data
+if config["retrieve_fmp_data"]:
+    fmp_quotes_raw = fmp_get_all_quotes(api_key_fmp)
+    if config["save_fmp_data"]: fmp_save_quotes_to_csv(fmp_quotes_raw, filename="fmp_quotes_raw")
+else:
+    fmp_quotes_raw = pd.read_csv("data/" + config["fmp_quotes_file"], index_col=0)
